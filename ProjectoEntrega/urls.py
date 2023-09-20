9ac3587af4dc18a96c1c63e9145d4515ddc7190e
nico.della <dellabernardanico@gmail.com>
2023-09-20 14:07:35 -0300

    fondo?

--- ProjectoEntrega/urls.py
+++ ProjectoEntrega/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app-entrega/', include('AppEntrega.urls'))
+    path('', include('AppEntrega.urls'))
 ]
